88493d22edc7d6da348fa6b62c04a28f6465ed95
moehzi <faizi1234098@gmail.com>
2021-01-15 19:57:18 +0800

    nav fix responsive

--- register/assets/js/script.js
+++ register/assets/js/script.js
@@ -1,6 +1,6 @@
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.links');
+	const burger = document.querySelector('.menu');
+	const nav = document.querySelector('.responsive-menu');
 	burger.addEventListener('click',()=>{
 		nav.classList.toggle('nav-active');
 		burger.classList.toggle('toggle');
